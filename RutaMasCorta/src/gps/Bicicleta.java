382e2ff3e0d0cdee93690d01bfc36f3ed81c9e7f
Jose Manuel <josemanuel.pla@gmail.com>
2018-05-06 12:20:31 +0200

    Añadimos dos for que faltaban para sumar las array sum2 y sum3

--- RutaMasCorta/src/gps/Bicicleta.java
+++ RutaMasCorta/src/gps/Bicicleta.java
@@ -22,7 +22,11 @@ public class Bicicleta {
         for(int i=0; i<camino1.length; i++)
         {
             sum1 += camino1[i];
+        }
+        for(int i=0; i<camino2.length; i++) {
             sum2 += camino2[i];
+        }
+        for(int i=0; i<camino3.length; i++) {
             sum3 += camino3[i];
         }
         
@@ -39,8 +43,7 @@ public class Bicicleta {
             if(sum1 == sum2 && sum2 == sum3)
             {
                 return "camino 1, 2 y 3";
-            }else if(sum1 == sum3)
-            {
+            }else if(sum1 == sum3){
                 return "caminos 1 y 3";
             }else if(sum2 == sum3){
                 return "caminos 2 y 3";
